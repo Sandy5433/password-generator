0f12ced050758a9563b884e3900b5a7fbf6233dd
Sandy Hung <sandyhung83@yahoo.com.tw>
2022-11-26 11:11:30 +1100

    arrays of password added

--- script.js
+++ script.js
@@ -30,7 +30,30 @@ function lowerCharacter = window.alert("Click OK to confirm including lowercase 
 function upperCharacter = window.alert("Click OK to confirm including uppercase characters"); //True or False
 
 //Aray of numeric options for computer to pick from
-var numOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var numOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Get random numeric number from array of options
+var numIndex = Math.floor(Math.random() * numOptions.length);
+var numChoice = numOption[numIndex];
+
+//Aray of uppercase character options for computer to pick from
+var upCharOption = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Get random uppercase character from array of options
+var upCharIndex = Math.floor(Math.random() * upCharOption.length);
+var upCharChoice = upCharOption[upCharIndex]
+
+//Aray of lowercase character options for computer to pick from
+var loCharOption = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Get random lowercase character from array of options
+var loCharIndex = Math.floor(Math.random() * loCharOption.length);
+var loCharChoice = loCharOption[loCharIndex];
+
+//Aray of special character options for computer to pick from
+var specialCharOption = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", ";", ":", "'", ",", ".", "/", "<", ">", "?"];
+// Get random special character from array of options
+var specialCharIndex = Math.floor(Math.random() * specialCharOption.length);
+var specialCharChoice = specialCharOption[specialCharIndex];
 
 
 
