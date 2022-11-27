e6837fb22b9f93e61fb35d5579e03e58bcf3c3f4
Sandy Hung <sandyhung83@yahoo.com.tw>
2022-11-27 13:51:29 +1100

    added condition for pw length

--- script.js
+++ script.js
@@ -3,22 +3,36 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
     // STEP 1: Getting user input
-    var passLength = prompt("What is the length of the password?");
+    var passLength = prompt("How many characters would you like your password to contain?");
 
     // validate the passLength if its > 8 or < 128
+    if(passLength <8) {
+        window.alert("Password length must be at least 8 characters")
+        return generatePassword()
+    }
+    if(passLength >128) {
+        window.alert("Password length must be less than 129 characters")
+        return generatePassword()
+    }
 
-    var includeLower = confirm("Do you want to include lowercase?");
-    var includeUpper = confirm("Do you want to include uppercase?");
+    var includeLower = confirm("Click OK to confirm including lowercase characters");
+    var includeUpper = confirm("Click OK to confirm including uppercase characters");
     // ask confirm question for numeric
+    var includenumeric = confirm("Click OK to confirm including numeric characters")
     // ask confirm question for special characters
+    var includespecial = confirm("Click OK to confirm including special characters");
 
+    console.log(passLength)
     console.log(includeLower)
     console.log(includeUpper)
-    console.log(passLength)
+    console.log(includenumeric)
+    console.log(includespecial)
 
     // STEP 2: determining what to include in the password
-    var lowerCharSet = "abcde";
-    var upperCharSet = "ABCDE";
+    var lowerCharSet = "abcdefghijklmnopqrstuvwxyz";
+    var upperCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numericSet = "0123456789";
+    var specialCharSet = "";
     var selections = "";
 
     if(includeLower) {
@@ -28,6 +42,19 @@ function generatePassword () {
     if(includeUpper) {
         selections = selections + upperCharSet
     }
+    if(includenumeric) {
+        selections = selections + numericSet
+    }
+
+    if(includespecial) {
+        selections = selections + specialCharSet
+    }
+
+    if ((includeLower && includeUpper && includenumeric && includespecial) == false) {
+        window.alert("You must select at least one character type")
+        return generatePassword()
+    }
+
     console.log(selections)
 
     // STEP 3: Generating the random password based on the selections
