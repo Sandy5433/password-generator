3735e3c62ad325b116c8b55da7585a55ab8efeb5
Sandy Hung <sandyhung83@yahoo.com.tw>
2022-12-01 22:17:14 +1100

    added README

---
+++ README.md
@@ -0,0 +1,37 @@
+# Password-Generator
+
+## Description
+
+In this project, the developer is given a starter code to build a browser application that will generate random passwords according to criterias specified by the user. In the process of building this application, developer can consolidate their learning of javascript functions taught in week 3 of coding bootcamp.
+
+You can access the application at 
+
+## Usage
+
+Below is a screenshot of the deployed webpage
+![alt="PasswordGenerator"](assets/images/pwdGen.png)
+
+Click on the 'Generate Password' button to enter your password inclusion criterias, such as password length and types of characters.
+This password generator can output passwords between 8 to 128 characters.  
+
+
+## Support
+
+Please reach out to Sandy Hung at sandyhung83@yahoo.com.tw if you have any questions or comments regarding this project. Alternatively, head on to my GitHub at https://github.com/Sandy5433 to see more exciting projects.
+
+## Roadmap
+
+New coding projects will be released every Wednesday night. My first project, refactoring html/css code, can be found on my GitHub.
+
+## Contributing
+
+If you would like to contribute to the development of this password generator application, you can reach out to me via the contact details on my portfolio webpage. 
+
+## Authors and acknowledgment
+
+Many thanks to my instructors and tutors from USYD coding bootcamp who stayed back after class to support me on my journey in coding.
+
+
+## Project status
+
+Completed 01/12/2022
--- script.js
+++ script.js
@@ -11,17 +11,20 @@ function generatePassword () {
         return generatePassword()
     }
     if(passLength >128) {
-        window.alert("Password length must be less than 129 characters")
+        window.alert("Password length must be no more than 128 characters")
         return generatePassword()
     }
 
+    // ask confirm question for lowercase
     var includeLower = confirm("Click OK to confirm including lowercase characters");
+    // ask confirm question for uppercase
     var includeUpper = confirm("Click OK to confirm including uppercase characters");
     // ask confirm question for numeric
     var includenumeric = confirm("Click OK to confirm including numeric characters")
     // ask confirm question for special characters
     var includespecial = confirm("Click OK to confirm including special characters");
 
+    // Log user choice
     console.log(passLength)
     console.log(includeLower)
     console.log(includeUpper)
@@ -29,32 +32,30 @@ function generatePassword () {
     console.log(includespecial)
 
     // STEP 2: determining what to include in the password
+    //List range of character type
     var lowerCharSet = "abcdefghijklmnopqrstuvwxyz";
     var upperCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numericSet = "0123456789";
-    var specialCharSet = "";
+    var specialCharSet = "~!@#$%^&*()-_+=[];:',./<>?";
     var selections = "";
 
     if(includeLower) {
         selections = selections + lowerCharSet
     }
-
     if(includeUpper) {
         selections = selections + upperCharSet
     }
     if(includenumeric) {
         selections = selections + numericSet
     }
-
     if(includespecial) {
         selections = selections + specialCharSet
     }
-
-    if ((includeLower && includeUpper && includenumeric && includespecial) == false) {
+    if ((includeLower || includeUpper || includenumeric || includespecial) == false) {
         window.alert("You must select at least one character type")
         return generatePassword()
     }
-
+    //Range of 
     console.log(selections)
 
     // STEP 3: Generating the random password based on the selections
